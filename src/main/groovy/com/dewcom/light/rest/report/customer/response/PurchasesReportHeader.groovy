ce7617aa76345615506b2bcc3f4eb68eb038f8e8
cdemoncr <chenmon.2092@gmail.com>
2018-02-22 12:22:03 -0600

    initial commit

--- src/main/groovy/com/dewcom/light/rest/report/customer/response/PurchasesReportHeader.groovy
+++
@@ -1,9 +0,0 @@
-package com.dewcom.light.rest.report.customer.response
-
-/**
- * Created by lchen on 9/10/17.
- */
-class PurchasesReportHeader {
-    String customerFullName = ""
-    String productName = ""
-}
